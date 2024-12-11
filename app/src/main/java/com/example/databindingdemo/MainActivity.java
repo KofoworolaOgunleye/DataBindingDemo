1d4e77fd27fbeec4812e3a00cd896df004eb83f6
Mohamed Fraitis <mfraitis1@gmail.com>
2024-12-11 11:21:28 +0000

    feat: add data binding to mainActivity

--- app/src/main/java/com/example/databindingdemo/MainActivity.java
+++ app/src/main/java/com/example/databindingdemo/MainActivity.java
@@ -7,14 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Person person = new Person("Simon", 21, "simon@nc.com");
+
+        activityMainBinding = (
+                DataBindingUtil.setContentView(
+                        this, R.layout.activity_main)
+                );
+
+        activityMainBinding.setPerson(person);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
